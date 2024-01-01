ffd544b5e4d974386eed8401696c03bb7e430cd3
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2024-01-02 05:30:22 +0545

    creating admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display=('name','description','is_completed')
+    list_filter=('is_completed',)
+    list_editable=('is_completed',)
+    search_fields=('name',)
--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,8 @@ class Task(models.Model):
     name=models.CharField(max_length=50)
     description=models.TextField(blank=True,null=True)
     is_completed=models.BooleanField(default=False)
+
+
+def __str__(self)->str:
+    return self.name
+
